c86deed46633328ad38231adc30e8d5d194564e4
leomet07 <leomet07@gmail.com>
2021-10-12 19:37:36 -0400

    Added coordinate support

--- server/model/Place.js
+++ server/model/Place.js
@@ -1,10 +1,15 @@
 const mongoose = require("mongoose");
 
 const placeSchema = new mongoose.Schema({
-	text: {
-		required: true,
+	name: {
 		type: String,
+		required: true,
 	},
+	long: {
+		type: Number,
+		required: true,
+	},
+	lat: { type: Number, required: true },
 });
 
 const placeModelfordb = mongoose.model("Place", placeSchema);
